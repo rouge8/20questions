701043506c37af72d8a022b200236acac642c34b
Andy Freeland <andy@andyfreeland.net>
2010-05-20 19:59:42 -0500

    psuedocode!

--- 20q.py
+++ 20q.py
@@ -3,8 +3,58 @@
 #
 #       20q.py
 
+'''DO WE WANT PROBABLY/DOUBTFUL OPTIONS?? WILL HAVE AN I DON'T KNOW'''
+
 import web
 import config, model
 
+def guess():
+    # return thing with highest weight from dictionary or top of heap
+    pass
+
+def ask_question():
+    # pop 50 top things from heap
+    # ask question such that it splits the data in halfish
+    # half get negative weights, half get positive weights
+    
+    pass
+
+def play_game():
+    # initialize heap WITH ALL WEIGHTS AT SOME VALUE OR ZERO
+    # ask AN INITIAL QUESTION (or more than one?)
+    # eg. plant/animal/thing or real/fictional, etc.
+    # then ask questions until 20 reached #### maybe in a function with parameter = number of questions to ask
+    #   ######################### this way if wrong: ask_questions(10) or something like that
+    #   get input from user
+    #   do magic
+    #       if it 'knows':
+    #           ask random questions for more info. (sneaky)
+    # when 20:
+    #   guess
+    #   right?
+    #       increase weights for object/questions (learn function)
+    #   wrong?
+    #       decrease weights for things
+    #       offer choices:
+    #           continue?
+    #               ask 10ish more questions? OR UNTIL VALUE REACHES SOMETHING? eg. it knows
+    #           tell answer?
+    #               add new thing, give it weights
+    #               also offer chance for new/useful question
+    #                   DON'T AUTOMATICALLY ADD THE QUESTION or obj
+    pass
+
+''' LEARNING ALGORITHM TIME
+inputs: object, set of questions, responses, correct/incorrect
+if correct:
+    increase weights by X ?????????????
+if wrong:
+    HALVE weights ??????????????? //can still have negative weights, this brings closer to zero
+    // if weight is 1, cut in half
+'''
+
+''' ASKING ALGORITHM
+'''
+
 if __name__ == '__main__':
-    print 'TWENTY FUCKING QUESTIONS BITCHES'
+    pass
--- model.py
+++ model.py
@@ -24,4 +24,24 @@ We'll want to do things like this managing the database, and just calling our
 functions from within the main program.
 '''
 
-pass
+def add_object(name):
+    # WHY THE RETURNS?
+    config.db.insert('objects', name=name)
+    # initialize weights for each question in data
+
+def add_question(question):
+    config.db.insert('questions', question=question)
+    # initialize weights for each object in data
+
+def update_data(object_id, question_id, value):
+    config.db.update('data', where='object_id = $object_id AND question_id = $questoin_id', vars=locals(), value=value)
+    # LOOK UP THE SYNTAX FOR THIS SO WE UNDERSTAND WHAT IS GOING ON
+    
+def update_weights(object_id, questions, values):
+    # list of questions by id eg. [1,2,3,5,61] with corresponding list
+    # of values eg. [100,2,5,6,14]
+    # MAYBE USE A DICTIONARY INSTEAD? Andy likes.
+    for i in range(len(questions)):
+        question_id = questions[i]
+        value = values[i]
+        update_data(object_id, question_id, value)
